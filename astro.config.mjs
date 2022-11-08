9779818586f1cf72d9997476bbd0aad6825897af
Neclo <josmo314@gmail.com>
2022-11-08 11:14:31 -0500

    Fixed non-loading Tailwind + added components

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
+
+import tailwind from "@astrojs/tailwind";
 import { defineConfig } from 'astro/config';
 
+
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  output: "server",
 });
