70438c40c9e3441348e3d2507e76f76d87d10c62
Neclo <josmo314@gmail.com>
2022-11-08 13:15:28 -0500

    Created Header and Ethos sections + Updated Layout

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,10 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
+		extend: {
+			colors: {
+				'mistblue': '#8087b0',
+			},
+		},
 	plugins: [],
 }
